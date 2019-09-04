5a7894c476692db5e6cdfa6aef35575886beb9ef
trcoo <trcooper1996@gmail.com>
2019-09-04 13:22:07 -0400

    Added prototypes for functions.

--- CommandLineParser.h
+++ CommandLineParser.h
@@ -5,4 +5,20 @@
 #ifndef CSUBATCH_COMMANDLINEPARSER_H
 #define CSUBATCH_COMMANDLINEPARSER_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int start_ui();
+void run(char*);
+void list();
+void fcfs();
+void sjf();
+void priority();
+void test();
+void quit();
+void help();
+typedef void (*command_array)();
+
+
 #endif //CSUBATCH_COMMANDLINEPARSER_H
