d3177880f0b097b1d459494b647ecd0b8162ec48
mmoore96 <38229603+mmoore96@users.noreply.github.com>
2019-09-06 17:25:07 -0400

    pthreads implemented

---
+++ GMM_code/Job.c
@@ -0,0 +1,9 @@
+//
+//  Job.c
+//  CSUBatch
+//
+//  Created by Michael Moore on 9/4/19.
+//  Copyright © 2019 George Moore. All rights reserved.
+//
+
+#include "Job.h"
---
+++ GMM_code/Scheduler.c
@@ -0,0 +1,17 @@
+//
+//  Scheduler.c
+//  CSUBatch
+//
+//  Created by Michael Moore on 9/4/19.
+//  Copyright © 2019 George Moore. All rights reserved.
+//
+
+#include "Scheduler.h"
+#include <stdlib.h>
+#include <pthread.h>
+
+
+void* schuduler_main (void *arg){
+    printf("schudluer worked!!!!\n");
+    return NULL;
+}
