9ba2621ac390e625cdc683ae667853f334391c34
George Moore <mmoore96@gmail.com>
2019-09-06 17:32:59 -0400

    Initial Commit

---
+++ CSUBatch/Dispatcher.h
@@ -0,0 +1,12 @@
+//
+//  Dispatcher.h
+//  CSUBatch
+//
+//  Created by Michael Moore on 9/4/19.
+//  Copyright © 2019 George Moore. All rights reserved.
+//
+
+#ifndef CSUBATCH_DISPATCHER_H
+#define CSUBATCH_DISPATCHER_H
+void* dispatcher_main (void *arg);
+#endif //CSUBATCH_DISPATCHER_H
